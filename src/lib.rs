5a39d725be86e0cc38f29994491b856da06148b4
David Hou <david.hou314@gmail.com>
2016-10-16 13:26:19 -0700

    nix-ify
    Delete everything that is already implemented in nix, including raw ptrace calls, ptrace request/option enumerations, and wait status parsing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![feature(step_by)]
 
 extern crate libc;
+pub extern crate nix;
 extern crate posix_ipc as ipc;
 #[macro_use] extern crate bitflags;
-#[macro_use] extern crate enum_primitive;
 
-use enum_primitive::FromPrimitive;
-use std::io;
+use nix::sys::ptrace;
+pub use nix::sys::ptrace::ptrace::*;
 use std::ptr;
 use std::default::Default;
 use std::vec::Vec;
@@ -14,54 +14,6 @@ use std::mem;
 
 pub type Address = u64;
 pub type Word = u64;
-
-#[derive(Clone, Copy)]
-pub enum Action {
-    Allow,
-    Kill
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Request {
-    TraceMe = 0,
-    PeekText = 1,
-    PeekData = 2,
-    PeekUser = 3,
-    PokeText = 4,
-    PokeData = 5,
-    PokeUser = 6,
-    Continue = 7,
-    Kill = 8,
-    SingleStep = 9,
-    GetRegs = 12,
-    SetRegs = 13,
-    Attach = 16,
-    Detatch = 17,
-    Syscall = 24,
-    SetOptions = 0x4200,
-    Seize = 0x4206
-}
-
-enum_from_primitive! {
-#[derive(Clone, Copy, Debug)]
-pub enum Event {
-  Fork = 1,
-  VFork = 2,
-  Clone = 3,
-  Exec = 4,
-  VForkDone = 5,
-  Exit = 6,
-  Seccomp = 7,
-  Stop = 128
-}
-}
-
-impl Event {
-    pub fn from_wait_status(st: i32) -> Option<Event> {
-        let e: Option<Event> = Event::from_i32(((st >> 8) & !5) >> 8);
-        return e;
-    }
-}
 
 #[derive(Clone, Copy, Default, Debug)]
 pub struct Registers {
@@ -94,102 +46,62 @@ pub struct Registers {
     pub gs: Word
 }
 
-bitflags! {
-  flags Options: u32 {
-    const SysGood = 1,
-    const TraceFork = 1 << 1,
-    const TraceVFork = 1 << 2,
-    const TraceClone = 1 << 3,
-    const TraceExec = 1 << 4,
-    const TraceVForkDone = 1 << 5,
-    const TraceExit = 1 << 6,
-    const TraceSeccomp = 1 << 7,
-    const ExitKill = 1 << 20
-  }
+fn ptrace_raw(request: PtraceRequest, pid: libc::pid_t, addr: *mut libc::c_void, data: *mut libc::c_void) -> Result<libc::c_long, i32> {
+    match ptrace::ptrace(request, pid, addr, data) {
+        Ok(v) => Ok(v),
+        Err(e) => match e {
+            nix::Error::Sys(errno) => Err(errno as i32),
+            nix::Error::InvalidPath => panic!("nix InvalidPath caught"),
+        },
+    }
 }
-
-fn os_errno() -> usize {
-    return io::Error::last_os_error().raw_os_error().unwrap_or(0) as usize;
-}
-
-pub fn setoptions(pid: libc::pid_t, opts: Options) -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::SetOptions, pid, ptr::null_mut(), opts.bits as *mut
-        libc::c_void)
+pub fn setoptions(pid: libc::pid_t, options: PtraceOptions) -> Result<(), i32> {
+    match ptrace::ptrace_setoptions(pid, options) {
+        Ok(v) => Ok(v),
+        Err(e) => match e {
+            nix::Error::Sys(errno) => Err(errno as i32),
+            nix::Error::InvalidPath => panic!("nix InvalidPath caught"),
+        },
     }
 }
 
-pub fn getregs(pid: libc::pid_t) -> Result<Registers, usize> {
+pub fn getregs(pid: libc::pid_t) -> Result<Registers, i32> {
     let mut buf: Registers = Default::default();
     let buf_mut: *mut Registers = &mut buf;
 
-    match unsafe {
-        raw(Request::GetRegs, pid, ptr::null_mut(), buf_mut as *mut libc::c_void)
-    } {
+    match ptrace_raw(PTRACE_GETREGS, pid, ptr::null_mut(), buf_mut as *mut libc::c_void) {
         Ok(_) => Ok(buf),
         Err(e) => Err(e)
     }
 }
 
-pub fn setregs(pid: libc::pid_t, regs: &Registers) -> Result<libc::c_long, usize> {
-    unsafe {
-        let buf: *mut libc::c_void = mem::transmute(regs);
-        raw(Request::SetRegs, pid, ptr::null_mut(), buf)
-    }
+pub fn setregs(pid: libc::pid_t, regs: &Registers) -> Result<libc::c_long, i32> {
+    let buf: *mut libc::c_void = unsafe { mem::transmute(regs) };
+    ptrace_raw(PTRACE_SETREGS, pid, ptr::null_mut(), buf)
 }
 
-pub fn seize(pid: libc::pid_t) -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::Seize, pid, ptr::null_mut(), ptr::null_mut())
-    }
+pub fn seize(pid: libc::pid_t) -> Result<libc::c_long, i32> {
+    ptrace_raw(PTRACE_SEIZE, pid, ptr::null_mut(), ptr::null_mut())
 }
 
-pub fn attach(pid: libc::pid_t) -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::Attach, pid, ptr::null_mut(), ptr::null_mut())
-    }
+pub fn attach(pid: libc::pid_t) -> Result<libc::c_long, i32> {
+    ptrace_raw(PTRACE_ATTACH, pid, ptr::null_mut(), ptr::null_mut())
 }
 
-pub fn release(pid: libc::pid_t, signal: ipc::signals::Signal) -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::Detatch, pid, ptr::null_mut(), (signal as u32) as *mut libc::c_void)
-    }
+pub fn release(pid: libc::pid_t, signal: ipc::signals::Signal) -> Result<libc::c_long, i32> {
+    ptrace_raw(PTRACE_DETACH, pid, ptr::null_mut(), (signal as u32) as *mut libc::c_void)
 }
 
-pub fn cont(pid: libc::pid_t, signal: ipc::signals::Signal) -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::Continue, pid, ptr::null_mut(), (signal as u32) as *mut libc::c_void)
-    }
+pub fn cont(pid: libc::pid_t, signal: ipc::signals::Signal) -> Result<libc::c_long, i32> {
+    ptrace_raw(PTRACE_CONT, pid, ptr::null_mut(), (signal as u32) as *mut libc::c_void)
 }
 
-pub fn cont_syscall(pid: libc::pid_t, signal: ipc::signals::Signal) -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::Syscall, pid, ptr::null_mut(), (signal as u32) as *mut libc::c_void)
-    }
+pub fn cont_syscall(pid: libc::pid_t, signal: ipc::signals::Signal) -> Result<libc::c_long, i32> {
+    ptrace_raw(PTRACE_SYSCALL, pid, ptr::null_mut(), (signal as u32) as *mut libc::c_void)
 }
 
-pub fn traceme() -> Result<libc::c_long, usize> {
-    unsafe {
-        raw(Request::TraceMe, 0, ptr::null_mut(), ptr::null_mut())
-    }
-}
-
-unsafe fn raw(request: Request,
-              pid: libc::pid_t,
-              addr: *mut libc::c_void,
-              data: *mut libc::c_void) -> Result<libc::c_long, usize> {
-    let v = ptrace(request as libc::c_int, pid, addr, data);
-    match v {
-        - 1 => Result::Err(os_errno()),
-        _ => Result::Ok(v)
-    }
-}
-
-extern {
-    fn ptrace(request: libc::c_int,
-              pid: libc::pid_t,
-              addr: *mut libc::c_void,
-              data: *mut libc::c_void) -> libc::c_long;
+pub fn traceme() -> Result<libc::c_long, i32> {
+    ptrace_raw(PTRACE_TRACEME, 0, ptr::null_mut(), ptr::null_mut())
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -201,7 +113,7 @@ pub struct Syscall {
 }
 
 impl Syscall {
-    pub fn from_pid(pid: libc::pid_t) -> Result<Syscall, usize> {
+    pub fn from_pid(pid: libc::pid_t) -> Result<Syscall, i32> {
         match getregs(pid) {
             Ok(regs) =>
                 Ok(Syscall {
@@ -214,7 +126,7 @@ impl Syscall {
         }
     }
 
-    pub fn write(&self) -> Result<libc::c_long, usize> {
+    pub fn write(&self) -> Result<libc::c_long, i32> {
         match getregs(self.pid) {
             Ok(mut regs) => {
                 regs.rdi = self.args[0];
@@ -249,10 +161,9 @@ impl Writer {
         }
     }
 
-    pub fn poke_data(&self, address: Address, data: Word) -> Result<Word, usize> {
-        match unsafe {
-            raw(Request::PokeData, self.pid, address as *mut libc::c_void, data as *mut libc::c_void)
-        } {
+    pub fn poke_data(&self, address: Address, data: Word) -> Result<Word, i32> {
+        match ptrace_raw(PTRACE_POKEDATA, self.pid, address as *mut libc::c_void,
+                             data as *mut libc::c_void) {
             Err(e) => Err(e),
             Ok(r) => Ok(r as Word)
         }
@@ -271,7 +182,7 @@ impl Writer {
         Ok(())
     }
 
-    pub fn write_data(&self, address: Address, buf: &Vec<u8>) -> Result<(), usize> {
+    pub fn write_data(&self, address: Address, buf: &Vec<u8>) -> Result<(), i32> {
         // The end of our range
         let max_addr = address + buf.len() as Address;
         // The last word we can completely overwrite
@@ -315,18 +226,14 @@ impl Reader {
         }
     }
 
-    pub fn peek_data(&self, address: Address) -> Result<Word, usize> {
-        let l;
-        unsafe {
-            l = raw(Request::PeekData, self.pid, address as *mut libc::c_void, ptr::null_mut())
-        }
-        match l {
-            Result::Err(e) => Result::Err(e),
-            _ => Result::Ok(l.unwrap() as Word)
+    pub fn peek_data(&self, address: Address) -> Result<Word, i32> {
+        match ptrace_raw(PTRACE_PEEKDATA, self.pid, address as *mut libc::c_void, ptr::null_mut()) {
+            Ok(v) => Ok(v as Word),
+            Err(e) => Err(e),
         }
     }
 
-    pub fn read_string(&self, address: Address) -> Result<Vec<u8>, usize> {
+    pub fn read_string(&self, address: Address) -> Result<Vec<u8>, i32> {
         let mut end_of_str = false;
         let mut buf: Vec<u8> = Vec::with_capacity(1024);
         let max_addr = address + buf.capacity() as Address;
